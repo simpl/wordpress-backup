712b3752d669d29784be4f5ad0f39447404bf71d
Marcus Clyne <maccaday@gmail.com>
2018-01-23 16:19:15 +0000

    Update WordPress code

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'wordpress' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'wordpress-pwd' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );

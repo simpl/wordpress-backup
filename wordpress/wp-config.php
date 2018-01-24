893b9fa74881d6b665543f556f32ecf541fb0c26
Marcus Clyne <maccaday@gmail.com>
2018-01-24 20:38:17 +0000

    Update config for multisite

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -92,6 +92,9 @@ define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
 
+/** Repairing DB tables **/
+define('WP_ALLOW_REPAIR', true);
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */

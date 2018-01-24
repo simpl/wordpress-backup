29be5b528344b264716715b49654bdc498a6741d
Marcus Clyne <maccaday@gmail.com>
2018-01-24 21:20:57 +0000

    Update config

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -88,7 +88,7 @@ define( 'FS_METHOD', 'direct');
 define( 'WP_ALLOW_MULTISITE', true );
 define( 'MULTISITE', true);
 define( 'SUBDOMAIN_INSTALL', true);
-define( 'DOMAIN_CURRENT_SITE', '');
+define( 'DOMAIN_CURRENT_SITE', 'mclyne.com');
 define( 'PATH_CURRENT_SITE', '/');
 define( 'SITE_ID_CURRENT_SITE', 1);
 define( 'BLOG_ID_CURRENT_SITE', 1);

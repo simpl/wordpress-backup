1559b84c2d0f9979b1664ae0d0fc8234ed195a97
Marcus Clyne <maccaday@gmail.com>
2018-01-24 20:31:43 +0000

    Re-install WP

--- wordpress/wp-content/themes/transcend/sidebar-footer.php
+++
@@ -1,5 +0,0 @@
-<section id="subfooter" class="subfooter secondary-color-bg dark">
-	<div class="container">
-		<?php do_action('cpotheme_subfooter'); ?>
-	</div>
-</section>
--- wordpress/wp-content/themes/transcend/sidebar.php
+++
@@ -1,8 +0,0 @@
-<?php $sidebar_config = cpotheme_get_sidebar_position(); ?>
-<?php if($sidebar_config != 'none'): ?>
-
-<aside id="sidebar" class="sidebar sidebar-primary">
-	<?php dynamic_sidebar('primary-widgets'); ?>
-</aside>
-
-<?php endif; ?>
